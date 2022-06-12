e0b557d3f27742d2cf8626668de25b6426a4f8a8
Quinn Keast <quinn@Quinns-MacBook-Pro.local>
2022-06-12 20:05:27 +0200

    Update fathom

--- components/meta.js
+++ components/meta.js
@@ -4,11 +4,6 @@ import { HOME_OG_IMAGE_URL, SITE_NAME } from "../lib/constants";
 export default function Meta() {
   return (
     <Head>
-      <script
-        src="https://stirring-ziggy-stardust.quinnkeast.com/script.js"
-        site={process.env.FATHOM}
-        defer
-      ></script>
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <title>{SITE_NAME}</title>
       <meta
